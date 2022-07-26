3c72a799f5617e5f219b461dd086214421a34510
Manjunath-Bhashyam <manjunathbhashyam1995@gmail.com>
2022-07-27 00:05:37 +0530

    app.py pdated

--- app.py
+++ app.py
@@ -201,4 +201,5 @@ def render_log_dir(req_path):
     return render_template('log_files.html', result=result)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.debug = True
+    app.run()
